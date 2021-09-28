534b557fc6855b63e0d163355566687df22463f2
scubbard <geomcf@gmail.com>
2021-09-28 14:15:45 -0400

    COND made text that changes with image

--- experiments/conditionals/js/script.js
+++ experiments/conditionals/js/script.js
@@ -6,6 +6,13 @@ georgie mcf
 
 "use strict";
 
+let georgieSmall = undefined;
+let georgieSmile = undefined;
+
+function preload() {
+  georgieSmall = loadImage('assets/images/georgieSMALL.png');
+  georgieSmile = loadImage('assets/images/georgieSMILE.png');
+}
 
 /**
 Description of setup
@@ -20,4 +27,24 @@ Description of draw()
 */
 function draw() {
   background(0);
+
+  imageMode(CENTER);
+  translate(width/2, height/2);
+  if (mouseX < width/2) {
+    scale(1, 1);
+    image(georgieSmile,0,0,300,300);
+    fill(255);
+    textSize(30);
+    textAlign(CENTER);
+    text('hello!', 0, 0);
+  } else {
+    scale(1, 1);
+    image(georgieSmall, 0, 0, 300, 300);
+    fill(255);
+    textSize(30);
+    textAlign(CENTER);
+    text('how are you!', 0, 0);
+  }
+
+  //image(georgieSmile, width/2, height/2)
 }
