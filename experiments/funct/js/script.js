631fafe9a35dc71e2f372ea09f63c781852ef700
scubbard <geomcf@gmail.com>
2021-10-07 02:59:58 -0400

    LOVE: debugged, added check for offscreen
    -debugged issue where circles wouldn't move due to unnecessary reset function in setup()

--- experiments/funct/js/script.js
+++ experiments/funct/js/script.js
@@ -15,7 +15,6 @@ let firstCirc = {
   vy: 0,
   size:100,
   speed: 5,
-  fill: 255,
 }
 
 let secondCirc = {
@@ -50,6 +49,7 @@ secondCirc.vy = random(-secondCirc.speed,secondCirc.speed);
 
 
 createCanvas(windowWidth,windowHeight);
+//reset();
 }
 
 
@@ -59,7 +59,13 @@ Description of draw()
 function draw() {
 
 
-background(255);
+  background(255);
+
+let offScreen = circleOffScreen();
+  if (offScreen) {
+  background(0);
+  }
+
 
 circle(firstCirc.x,firstCirc.y,firstCirc.size);
   firstCirc.x = firstCirc.x + firstCirc.vx;
@@ -69,4 +75,26 @@ circle(secondCirc.x,secondCirc.y,secondCirc.size);
   secondCirc.x = secondCirc.x + secondCirc.vx;
   secondCirc.y = secondCirc.y + secondCirc.vy;
 
+}
+
+
+function circleOffScreen() {
+  let result = (firstCirc.x > width || firstCirc.x < 0 || firstCirc.y > height ||
+      firstCirc.y < 0 || secondCirc.x > width || secondCirc.x < 0 ||
+      secondCirc.y > height || secondCirc.y < 0);
+  print(result);
+  return result;
+}
+
+
+function reset() {
+  firstCirc.x = width/3;
+  firstCirc.y = height/2;
+  firstCirc.vx = 0;
+  firstCirc.vy = 0;
+
+  secondCirc.x = width/3 + width/3;
+  secondCirc.y = height/2;
+  secondCirc.vx = 0;
+  secondCirc.vy = 0;
 }
