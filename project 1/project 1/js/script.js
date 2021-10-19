fa14a204d3b50581a054b2e4f7280676cfc3f80a
scubbard <geomcf@gmail.com>
2021-10-19 16:08:34 -0400

    p1: added title state

--- project 1/project 1/js/script.js
+++ project 1/project 1/js/script.js
@@ -8,20 +8,14 @@ author, and this description to match your project!
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
+let state = `title`
 
 /**
 Description of setup
 */
 function setup() {
-
+createCanvas(windowWidth,windowHeight);
+background(200);
 }
 
 
@@ -29,5 +23,12 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  if (state === `title`) {
+    title(); }
+}
 
+function title() {
+  background(0,200,150);
+  stroke(0);
+  text(`click to start`,width/2,height/2);
 }
