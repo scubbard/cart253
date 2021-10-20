90eb609704ae6d53dd0a935dfda1d858348a91d6
scubbard <geomcf@gmail.com>
2021-10-19 21:35:22 -0400

    p1: added player + other objects

--- project 1/project 1/js/script.js
+++ project 1/project 1/js/script.js
@@ -1,21 +1,48 @@
 /**
-Title of Project
-Author Name
+music is good!
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+by georgie
+
+short simulation. collect the music notes and win a prize (it's more music)!
 */
 
 "use strict";
 
 let state = `title`
 
+let player = {
+  x: undefined,
+  y: undefined,
+  vx: 0,
+  vy: 0,
+  size:50,
+  speed: 5,
+}
+
+let moverOne = {
+  x: undefined,
+  y: undefined,
+  vx: 0,
+  vy: 0,
+  size:50,
+  speed: 5,
+}
+
+let moverTwo = {
+  x: undefined,
+  y: undefined,
+  vx: 0,
+  vy: 0,
+  size:50,
+  speed: 5,
+}
+
 /**
 Description of setup
 */
 function setup() {
 createCanvas(windowWidth,windowHeight);
-background(200);
+firstPosition();
 }
 
 
@@ -25,10 +52,59 @@ Description of draw()
 function draw() {
   if (state === `title`) {
     title(); }
+  if (state === `simulation`) {
+    simulation();
+  }
+}
+
+function simulation(){
+  display();
+}
+
+function display() {
+  stroke(0);
+  circle(player.x,player.y,player.size);
 }
 
 function title() {
   background(0,200,150);
   stroke(0);
   text(`click to start`,width/2,height/2);
+}
+
+function display(){
+  push();
+  stroke(0);
+  fill(0);
+  circle(player.x,player.y,player.size);
+  pop();
+  push();
+  stroke(0);
+  fill(200,0,50);
+  circle(moverOne.x,moverOne.y,moverOne.size);
+  circle(moverTwo.x,moverTwo.y,moverTwo.size);
+  pop();
+}
+
+function firstPosition(){
+  player.x = width/2;
+  player.y = height/2;
+  player.vx = random(-5,5);
+  player.vy = random(-5,5);
+
+  moverOne.x = width/4
+  moverOne.y = height/2
+  moverOne.vx = random(-2,2);
+  moverOne.vy = random(-2,2);
+
+  moverTwo.x = width - width/4
+  moverTwo.y = height/2
+  moverTwo.vx = random(-2,2);
+  moverTwo.vy = random(-2,2);
+}
+
+function mousePressed() {
+  if (state === `title`) {
+    state = `simulation`;
+  }
 }
