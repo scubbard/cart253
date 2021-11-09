262927169bf1fa3cd1127517b1282c10c395183a
scubbard <geomcf@gmail.com>
2021-11-09 18:28:11 -0500

    p2Proto:fixed some git issues, created prototype document

---
+++ p2 prototype/js/script.js
@@ -0,0 +1,25 @@
+/**
+what type of song is this? prototype
+code by georgie
+*/
+
+"use strict";
+
+
+/**
+Description of setup
+*/
+function setup() {
+createCanvas(windowWidth,windowHeight);
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+background(200,50,50);
+type(`hello!`,width/2,height/2);
+rect(200,200,200,);
+}
