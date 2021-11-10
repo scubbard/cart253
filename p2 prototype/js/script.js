81128a6ecd94c6d53445e1d1597be6798459c8ae
scubbard <geomcf@gmail.com>
2021-11-09 22:29:19 -0500

    p2 proto: added states, typing capabilities, ending conditions

--- p2 prototype/js/script.js
+++ p2 prototype/js/script.js
@@ -5,7 +5,8 @@ code by georgie
 
 "use strict";
 let myFont;
-
+let typing = ``;
+let state = `start`
 
 function preload() {
   myFont = loadFont(`assets/HappyTime.otf`);
@@ -26,16 +27,44 @@ createCanvas(windowWidth,windowHeight);
 Description of draw()
 */
 function draw() {
-background(200,50,50);
-stroke(0);
-textFont(myFont);
-textSize(100);
-text(key,width/2,height/2);
+  if (state === `start`) {
+    simulation();
+  }
+  if (state === `ending`) {
+    ending();
+  }
 
+wordTyped();
+}
 
+function simulation(){
+  background(200,50,50);
+  stroke(0);
+  textFont(myFont);
+  textSize(100);
+  textAlign(CENTER);
+  text(typing,width/2,height/2);
 }
 
 function keyTyped() {
   stroke(0);
-  text(key);
+  typing = typing + key;
+}
+//delete function is not currently working. need to debug
+function keyPressed() {
+  if (keyCode === BACKSPACE){
+    typing = typing - key;
+    print(typing);
+  }
+}
+
+function wordTyped() {
+  if (typing === `testing`){
+  state = `ending`}
+}
+
+function ending() {
+  background(50,200,0);
+  text(`wahoo!`, width/2,height/2)
+
 }
