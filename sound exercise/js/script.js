aeb52dea32e476abc117f46d6e19a83b10ab6194
scubbard <geomcf@gmail.com>
2021-11-15 22:14:07 -0500

    ex. make noise: added mouse tracker test

--- sound exercise/js/script.js
+++ sound exercise/js/script.js
@@ -35,4 +35,29 @@ Description of draw()
 */
 function draw() {
   background(0);
+  imageMode(CENTER);
+  mouseWhere();
+}
+
+function mouseWhere() {
+  if (mouseX<width/3) {
+    leftImage();
+  }
+  else if (mouseX>width - width/3) {
+    rightImage();
+  }
+  else {
+    centerImage();
+  }
+}
+
+function leftImage() {
+    image(leftSprite,width/3,height/2);
+  }
+function centerImage() {
+  image(centerSprite,width/2,height/2);
+}
+
+function rightImage() {
+  image(rightSprite,width - width/3,height/2);
 }
