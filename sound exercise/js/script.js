5ada45a9f0f7c9f17ca876acb37b3d03e98c6b08
scubbard <geomcf@gmail.com>
2021-11-16 19:39:09 -0500

    ex. make noise: used secondary variable to simplify sprite display, added music

--- sound exercise/js/script.js
+++ sound exercise/js/script.js
@@ -8,15 +8,22 @@ author, and this description to match your project!
 
 "use strict";
 
-let leftSprite = undefined;
-let centerSprite = undefined;
-let rightSprite = undefined;
-let leftText = undefined;
-let rightText = undefined;
-let yeahText = undefined;
+let state = `start`;
 
-let leftImageShowing = false;
-let rightImageShowing = false;
+let leftSprite;
+let rightSprite;
+let centerSprite;
+
+let leftText;
+let rightText;
+let yeahText;
+
+let currentSprite = centerSprite;
+let currentText = yeahText;
+
+
+let layerOne;
+
 let centerImageShowing = false;
 
 /**
@@ -29,6 +36,8 @@ function preload() {
   rightText = loadImage(`assets/images/rightText.png`);
   leftText = loadImage(`assets/images/leftText.png`);
   yeahText = loadImage(`assets/images/yeahText.png`);
+
+  layerOne = loadSound(`assets/sounds/songLayerFast.mp3`)
 }
 
 
@@ -44,41 +53,52 @@ function setup() {
 Description of draw()
 */
 function draw() {
-  background(0);
+  if (state === `start`) {
+    start();
+  }
 
-  //mouseWhere();
-  keyPressed();
-  keyReleased();
+  if (state === `simulation`) {
+    simulation();
+  }
+}
+function handleInput() {
+  if (keyIsDown(LEFT_ARROW)) {
+    currentSprite = leftSprite;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    currentSprite = rightSprite;
+  } else {
+    currentSprite = centerSprite;
+  }
 }
 
 function keyPressed() {
   imageMode(CENTER);
   showLeft();
   showRight();
-  checkCenter();
+  if (keyCode === DOWN_ARROW) {
+    layerOne.stop();
+  }
+  //checkCenter();
 }
 
 function keyReleased() {
   if (keyCode === LEFT_ARROW || RIGHT_ARROW) {
     showCenter();
-    rightIsShowing = false
-    leftIsShowing = false
   }
 }
 
 function showLeft() {
-  if (keyIsDown(LEFT_ARROW) && !rightImageShowing) {
+  if (keyIsDown(LEFT_ARROW)) {
     reset();
     leftImage();
-    leftIsShowing = true
+    song();
     }
   }
 
 function showRight() {
-  if (keyIsDown(RIGHT_ARROW) && !leftImageShowing) {
+  if (keyIsDown(RIGHT_ARROW)) {
     reset();
     rightImage();
-    rightIsShowing = true
   }
 }
 
@@ -88,12 +108,37 @@ function showCenter() {
   centerImageShowing = true
 }
 
+function mousePressed() {
+  if (state === `start`) {
+    state = `simulation`;
+  }
+}
+
+/**
 function checkCenter() {
   if (!leftImageShowing || !rightImageShowing) {
     showCenter();
   }
 }
+*/
 
+function start(){
+  background(200,0,70);
+  textSize(50);
+  text(`click to start`,width/2,height/2)
+}
+
+function simulation(){
+  background(0);
+  textAlign(CENTER);
+  stroke(255);
+  fill(200,150,0);
+  text(`use left and right arrow keys to dance!`,width/2,height - height/5);
+  image(currentSprite);
+  handleInput();
+  myText();
+
+}
 
 function leftImage() {
     image(leftSprite,width/3,height/2);
@@ -110,9 +155,18 @@ function rightImage() {
   image(rightText,width - width/3, height - height/4)
 }
 
+
+function song() {
+  if(!layerOne.isPlaying()){
+    layerOne.loop();
+  }
+}
+
 function reset() {
   background(0);
   //checkCenter();
-  leftImageShowing = false
-  rightImageShowing = false
+}
+
+function myText(){
+  text(currentText,currentText.x,currentText.y);
 }
